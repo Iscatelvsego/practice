c53dd3c0e923d23750a2cb63def36b6598ad299a
Iscatelvsego <51111303+Iscatelvsego@users.noreply.github.com>
2019-05-31 12:10:57 +0800

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+
+
+class Rextester
+    
+{  
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World!");
+        System.out.println(new Date());
+         int x=1, y=2, z =3;
+	int a=x+y+z;        
+	int b=x+(y-2)/(2+z);
+        System.out.println(" a = " + a + " b = " + b);
+    }
+}
